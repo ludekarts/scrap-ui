4614bd5becb2099b71fff8d10429bfa8130ea7c5
ludekarts <ludekarts@gmail.com>
2024-12-17 12:29:01 +0100

    Setup types export

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Hello } from "scrap-ui";
+import { Hello } from "@ludekarts/scrap-ui";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     // library entry and output settings
     lib: {
